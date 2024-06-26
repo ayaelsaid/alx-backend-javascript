7440aa48e059a0c4041648628a92931faa51899b
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-06-27 02:25:07 +0300

    Update 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,4 +1,4 @@
-function handleResponseFromAPI(promise) {
+export default function handleResponseFromAPI(promise) {
     return promise.then(() => {
       return { status: 200, body: 'success' };
     }).catch(() => {
