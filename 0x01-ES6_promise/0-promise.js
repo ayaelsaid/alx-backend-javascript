6d7f86e2395a186ccc5a3a6a5d856e49a9ee8c4a
ayaelsaid <ayae7675@gmail.com>
2024-06-26 23:16:16 +0000

    my first

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,9 @@
+function getResponseFromAPI() {
+    return new Promise ((resolve, reject) => {
+    //      let sucsses = true;
+    //  if(sucsses)
+    //      resolve('yes')
+    //  else 
+    //  reject('no')
+     })
+ }
