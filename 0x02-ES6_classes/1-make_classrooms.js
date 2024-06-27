659e2f998f9e10fb50a7dd19ebc79014c6b23321
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-06-28 02:34:23 +0300

    Update 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,11 +1,11 @@
-import ClassRoom from './0-classroom.js';
+import Classroom from './0-classroom';
 
-export function initializeRooms() {
-  const rooms = [
-    new ClassRoom(19),
-    new ClassRoom(20),
-    new ClassRoom(34)
+function initializeRooms() {
+  return [
+    new Classroom(19),
+    new Classroom(20),
+    new Classroom(34),
   ];
-
-  return rooms;
 }
+
+export default initializeRooms;
