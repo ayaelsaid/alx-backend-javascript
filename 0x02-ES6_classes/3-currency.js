377554cb7cca41aa156613c5942350668f0d6021
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-06-28 02:55:20 +0300

    Create 3-currency.js

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,42 @@
+// Currency.js
+
+class Currency {
+  constructor(code, name) {
+    if (typeof code !== 'string') {
+      throw new Error('Code must be a string');
+    }
+    if (typeof name !== 'string') {
+      throw new Error('Name must be a string');
+    }
+    this._code = code;
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new Error('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    if (typeof newCode !== 'string') {
+      throw new Error('Code must be a string');
+    }
+    this._code = newCode;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
+
+export default Currency
