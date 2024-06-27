e8a76e2f6786cfe5372e3500ecadc7866428a194
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-06-27 03:37:24 +0300

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,3 @@
 export default function getResponseFromAPI() {
-    return new Promise ((resolve, reject) => {
-    //      let sucsses = true;
-    //  if(sucsses)
-    //      resolve('yes')
-    //  else 
-    //  reject('no')
-     })
+    return new Promise ((resolve, reject) => {})
  }
