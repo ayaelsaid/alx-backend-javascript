4c05676df5edd93b2bd848a1c4bcbca4d944254c
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-06-27 04:42:09 +0300

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,15 @@
-import signUpUser from "./4-user-promise";
+import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  let promiseOne = signUpUser(firstName, lastName);
-  let promiseTwo = uploadPhoto(fileName);
-  
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const promiseOne = signUpUser(firstName, lastName);
+  const promiseTwo = uploadPhoto(fileName);
+
   return Promise.allSettled([promiseOne, promiseTwo]).then((results) => {
     return results.map((result) => {
       return {
         status: result.status,
-        value: result.status === 'fulfilled' ? result.value : result.reason,
+        value: result.status === 'fulfilled' ? result.value : String(result.reason),
       };
     });
   });
