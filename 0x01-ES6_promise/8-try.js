fba16ee579004359ea5f4874207cdc02ee488d4b
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-06-27 05:18:01 +0300

    Update 8-try.js

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,8 +1,7 @@
 export default function divideFunction(numerator, denominator) {
-    if (denominator !== 0) {
-        let result = numerator / denominator;
-        return result;
-    } else {
-        throw Error('Cannot divide by 0');
-    }
+  if (denominator === 0) {
+    throw Error('cannot divide by 0');
+  } else {
+    return numerator / denominator;
+  }
 }
