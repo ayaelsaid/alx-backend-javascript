9691551ed0c9d0dccb883cbf804136a78b0cf9cc
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-06-27 05:56:37 +0300

    Update 9-try.js

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -6,7 +6,7 @@ export default function guardrail(mathFunction) {
     } catch (error) {
         queue.push(error.message);
     } finally {
-        queue.push('Guardrail was processed');e
+        queue.push('Guardrail was processed');
     }
 
     return queue;
