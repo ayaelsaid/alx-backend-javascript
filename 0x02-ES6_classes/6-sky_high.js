414e174511e0b781e8c69d707e16276ce318c2fb
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-06-28 04:46:40 +0300

    Update 6-sky_high.js

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -2,11 +2,11 @@ import Building from './5-building.js';
 
 class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
-    super(sqft);
     if (typeof floors !== 'number') {
       throw new Error('floors must be a number');
     }
     this._floors = floors;
+    super(sqft);
   }
 
   get floors() {
