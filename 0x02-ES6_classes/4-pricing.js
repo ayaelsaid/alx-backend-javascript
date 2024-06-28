8d562dfb01530d75ca49fe181068fe0a19e83bb5
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-06-28 03:09:33 +0300

    Create 4-pricing.js

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,49 @@
+import Currency from './3-currency.js';
+
+class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw new Error('Amount must be a number');
+    }
+    if (!(currency instanceof Currency)) {
+      throw new Error('Currency must be an instance of the Currency class');
+    }
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    if (typeof newAmount !== 'number') {
+      throw new Error('Amount must be a number');
+    }
+    this._amount = newAmount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    if (!(newCurrency instanceof Currency)) {
+      throw new Error('Currency must be an instance of the Currency class');
+    }
+    this._currency = newCurrency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new Error('Both amount and conversion rate must be numbers');
+    }
+    return amount * conversionRate;
+  }
+}
+
+export default Pricing;
