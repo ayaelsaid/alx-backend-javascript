bb3e9e1186f6e874a646daf583b9cb027bbe3166
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-06-28 05:02:41 +0300

    Update 6-sky_high.js

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -10,8 +10,22 @@ export default class SkyHighBuilding extends Building {
     return this._sqft; // Inherited from Building
   }
 
+  set sqft(newSqft) {
+    if (typeof newSqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+    this._sqft = newSqft;
+  }
+
   get floors() {
     return this._floors;
+  }
+
+  set floors(newFloors) {
+    if (typeof newFloors !== 'number') {
+      throw new TypeError('floors must be a number');
+    }
+    this._floors = newFloors;
   }
 
   evacuationWarningMessage() {
