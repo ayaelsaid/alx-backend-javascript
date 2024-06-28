795984f8ea1e84e1c03c7409d20c697157d2c670
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-06-28 05:00:35 +0300

    Update 6-sky_high.js

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -2,16 +2,16 @@ import Building from './5-building.js';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
-    super(sqft);
+    super(sqft); // Initialize sqft using the parent class constructor
     this._floors = floors;
+  }
+
+  get sqft() {
+    return this._sqft; // Inherited from Building
   }
 
   get floors() {
     return this._floors;
-  }
-
-  set floors(newFloors) {
-    this._floors = newFloors;
   }
 
   evacuationWarningMessage() {
