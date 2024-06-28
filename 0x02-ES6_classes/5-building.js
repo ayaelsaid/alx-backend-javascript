a9aaf2b8e3e17893106f867c7f8f6c6317b2e8a4
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-06-28 04:35:41 +0300

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -5,22 +5,23 @@ class Building {
     }
     this._sqft = sqft;
 
-    if (this.constructor !== Building) {
+    if (this.constructor === Building) {
       if (typeof this.evacuationWarningMessage !== 'function') {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
     }
   }
 
   get sqft() {
     return this._sqft;
   }
-}
 
-set sqft(newSqft) {
-  if (typeof newSqft !== 'number') {
+  set sqft(newSqft) {
+    if (typeof newSqft !== 'number') {
       throw new Error('sqft must be a number');
     }
-      this._sqft = newSqft;
+    this._sqft = newSqft;
+  }
 }
 
 export default Building;
