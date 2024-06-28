2481117415184e0b50dd2f78dab486aa81806745
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-06-28 04:26:00 +0300

    Update 8-hbtn_class.js

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -14,8 +14,22 @@ class HolbertonClass {
     return this._size;
   }
 
+  set size(newSize) {
+    if (typeof newSize !== 'number') {
+      throw new Error('Size must be a number');
+    }
+    this._size = newSize;
+  }
+
   get location() {
     return this._location;
+  }
+
+  set location(newLocation) {
+    if (typeof newLocation !== 'string') {
+      throw new Error('Location must be a string');
+    }
+    this._location = newLocation;
   }
 
   [Symbol.toPrimitive](hint) {
