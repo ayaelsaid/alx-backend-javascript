aa1a17c41a2c32c8a371b15751c7e69495deb3b5
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-06-29 01:41:33 +0300

    Update 11-createEmployeesObject.js

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,6 +1,8 @@
 export default function createEmployeesObject(departmentName, employees) {
   const obj = {
-    [departmentName]: [...employees]  // Using spread syntax to create a new array
+    [departmentName]: [
+      ...employees,
+    ]
   };
   return obj;
 }
