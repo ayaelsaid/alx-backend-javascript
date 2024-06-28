af7937f20e572d732c827c6704d44200b898b971
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-06-28 03:54:45 +0300

    Create 8-hbtn_class.js

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,31 @@
+class HolbertonClass {
+  constructor(size, location) {
+    if (typeof size !== 'number') {
+      throw new Error('Size must be a number');
+    }
+    if (typeof location !== 'string') {
+      throw new Error('Location must be a string');
+    }
+    this._size = size;
+    this._location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this._size;
+    } else if (hint === 'string' || hint === 'default') {
+      return this._location;
+    }
+    return this._location;
+  }
+}
+
+export default HolbertonClass;
