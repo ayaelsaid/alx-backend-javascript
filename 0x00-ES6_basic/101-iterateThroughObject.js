db6c04968ec28724cc616a37c3419f6084016dd8
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-06-29 20:44:15 +0300

    Update 101-iterateThroughObject.js

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,7 +1,6 @@
 export default function iterateThroughObject(reportWithIterator) {
   let newValue = '';
   for (const value of reportWithIterator) {
-    if (value !== reportWithIterator.slice(-1)[0]) {
       newValue +=  value !== reportWithIterator.slice(-1)[0] ? value + ' | ' : value; 
   }
   return newValue;
