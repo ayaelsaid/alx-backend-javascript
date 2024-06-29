79ae06629e5c294646a76b979eace21d8c576f6a
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-06-29 03:08:24 +0300

    Update 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,5 +1,3 @@
-import createReportObject from './12-createReportObject';
-
 export default function createIteratorObject(report) {
   const newArray = [];
   const values = Object.values(report.allEmployees);
