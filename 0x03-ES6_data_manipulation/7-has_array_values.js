fff785003720655c7d9e66798a5dfbfa30a5f5f0
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-07-03 02:17:48 +0300

    Create 7-has_array_values.js

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,11 @@
+export default function hasValuesFromArray(set, array) {
+    // Iterate over each element in the array
+    for (let item of array) {
+        // Check if the item exists in the set
+        if (!set.has(item)) {
+            return false;
+        }
+    }
+    // If all elements are found in the set, return true
+    return true;
+}
