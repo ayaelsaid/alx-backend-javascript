27ca9fbf9be0b40a26da1f2a7abd5765efab2afa
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-07-03 05:00:18 +0300

    Update 10-update_uniq_items.js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,8 @@
 export default function updateUniqueItems(map) {
+        if (!(map instanceof Map)) {
+        throw new Error('Cannot process');
+    }
+
     for (let [key, value] of map.entries()) {
         if (value === 1) {
             map.set(key, 100);
