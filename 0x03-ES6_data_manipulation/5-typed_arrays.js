978802fe9e169dc880ce992b3e2b5fb86cb0af97
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-07-03 07:00:00 +0300

    Update 5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,14 +1,17 @@
-export default function createInt8TypedArray(length, position, value) {
-  // Initialize an Int8Array with the specified length
-  const int8Array = new ArrayBuffer(length);
+function createInt8TypedArray(length, position, value) {
+  // Initialize an ArrayBuffer with the specified length
+  const buffer = new ArrayBuffer(length);
+  const int8Array = new Int8Array(buffer);
 
   // Check if the position is outside the range of the array
-  if (position >= length || position < 0) {
+  if (position >= int8Array.length || position < 0) {
     throw new Error('Position outside range');
   }
-  // Set the value at the specified position
 
-  const dataView = new DataView(int8Array);
+  // Create a DataView of the buffer
+  const dataView = new DataView(buffer);
+
+  // Set the value at the specified position
   dataView.setInt8(position, value);
 
   return dataView;
