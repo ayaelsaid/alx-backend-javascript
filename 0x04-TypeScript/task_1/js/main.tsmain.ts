6a799121f65973eda73f658789af9aadb99d0b6d
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-07-04 02:51:22 +0300

    Update main.tsmain.ts

--- 0x04-TypeScript/task_1/js/main.tsmain.ts
+++ 0x04-TypeScript/task_1/js/main.tsmain.ts
@@ -1,4 +1,4 @@
-interface Teacher {
+export interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
   fullTimeEmployee: boolean;
@@ -7,6 +7,39 @@ interface Teacher {
   [prop: string]: any; // Allow any additional properties
 }
 
-interface Directors extends Teacher {
+export interface Directors extends Teacher {
   numberOfReports: number;
+}
+
+export interface PrintTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+export const printTeacher: PrintTeacherFunction = (firstName, lastName) => `${firstName.charAt(0)}. ${lastName}`;
+
+export interface StudentClassConstructor {
+  new (firstName: string, lastName: string): StudentClassI;
+}
+
+export interface StudentClassI {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+export class StudentClass implements StudentClassI {
+  private _firstName: string;
+  private _lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+  }
+
+  workOnHomework() {
+    return 'Currently working';
+  }
+
+  displayName() {
+    return this._firstName;
+  }
 }
