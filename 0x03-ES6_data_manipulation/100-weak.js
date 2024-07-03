5c9248e8c8ed7d26016fb036ab8a89002812684b
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-07-03 08:24:27 +0300

    Update 100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,15 +1,19 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
+  // Check if the endpoint is already in the weakMap
   if (!weakMap.has(endpoint)) {
     weakMap.set(endpoint, 1);
-  }
-  const callCount = weakMap.get(endpoint);
-  weakMap.set(endpoint, callCount + 1);
+  } else {
+    const callCount = weakMap.get(endpoint);
+    // Update the call count in the weakMap
+    weakMap.set(endpoint, callCount + 1);
 
-  if (callCount >= 4) {
-    throw new Error('Endpoint load is high.');
+    // Throw an error if the call count exceeds the limit
+    if (callCount >= 4) {
+      throw new Error('Endpoint load is high.');
+    }
   }
 
-  return callCount;
+  return weakMap.get(endpoint);
 }
