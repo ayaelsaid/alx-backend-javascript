ba3b7f64fc1a9485baeb7e34d0273f69d8e1f5af
ayaelsaid <ayae7675@gmail.com>
2024-07-03 03:46:30 +0000

    update

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -4,10 +4,10 @@ export default function queryAPI(endpoint) {
   // Retrieve the current call count for the endpoint from weakMap
   let callCount = weakMap.get(endpoint) || 0;
 
- if (!weakMap.has(endpoint)) {
-   weakMap.set(endpoint, 0)
- }
- callCount = weakMap.get(endpoint) + 1;
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+  callCount = weakMap.get(endpoint) + 1;
   if (callCount >= 5) {
     throw new Error('Endpoint load is high.');
   }
