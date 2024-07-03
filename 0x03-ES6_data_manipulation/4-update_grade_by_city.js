3b26797938a6598ddbe8ec409d2fba20dd29a53f
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-07-03 05:41:27 +0300

    Update 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,7 +2,7 @@ export default function updateStudentGradeByCity(students, city, newGrdes) {
   if (!Array.isArray(students)) {
     return 'Students should be a list'; // Return a message indicating students should be an array
   }
-  if (typeof (city) !== 'string') {
+  if (typeof city !== 'string') {
     return 'City must be a string';
   }
   if (!Array.isArray(newGrdes)) {
@@ -15,12 +15,10 @@ export default function updateStudentGradeByCity(students, city, newGrdes) {
       const grade = newGrdes.find((grade) => grade.studentId === student.id);
       if (grade) {
         return { ...student, grade: grade.grade }; // Use spread operator to avoid mutation
-      } else {
-        return { ...student, grade: 'N/A' }; // Use spread operator to avoid mutation
-
       }
-      return student;
+      return { ...student, grade: 'N/A' }; // Use spread operator to avoid mutation
     });
 
   return specificCity;
 }
+
