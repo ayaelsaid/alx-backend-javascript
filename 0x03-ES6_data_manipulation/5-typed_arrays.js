aab4478dd56be11a5ac09946736c732ea514f0c8
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-07-03 06:00:09 +0300

    Update 5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -4,7 +4,7 @@ export default function createInt8TypedArray(length, position, value) {
   const int8Array = new Int8Array(buffer);
 
   // Check if the position is outside the range of the array
-  if (position >= int8Array.length || position < 0) {
+  if (position > int8Array.length || position < 0) {
     return 'Position outside range';
   }
   int8Array[position] = value;
