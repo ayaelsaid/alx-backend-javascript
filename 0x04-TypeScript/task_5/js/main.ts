a76217a0970efcca84b3897695389a00c5cb5158
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-07-04 05:54:44 +0300

    Update main.ts

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -1,27 +1,22 @@
-// task_5/js/main.ts
-
-// Define the MajorCredits interface
-interface MajorCredits {
+export interface MajorCredits {
   credits: number;
   brand: 'MajorCredits';
 }
 
 // Define the MinorCredits interface
-interface MinorCredits {
+export interface MinorCredits {
   credits: number;
   brand: 'MinorCredits';
 }
 
-// Function to sum MajorCredits
-function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
-  return {
-    credits: subject1.credits + subject2.credits as MajorCredits,
-  };
-}
+// Function to sum MajorCredits using arrow function
+export const sumMajorCredits = (subject1: MajorCredits, subject2: MajorCredits): MajorCredits => ({
+  credits: subject1.credits + subject2.credits,
+  brand: 'MajorCredits'
+} as MajorCredits);
 
-// Function to sum MinorCredits
-function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
-  return {
-    credits: subject1.credits + subject2.credits as MinorCredits
-  };
-}
+// Function to sum MinorCredits using arrow function
+export const sumMinorCredits = (subject1: MinorCredits, subject2: MinorCredits): MinorCredits => ({
+  credits: subject1.credits + subject2.credits,
+  brand: 'MinorCredits'
+} as MinorCredits);
