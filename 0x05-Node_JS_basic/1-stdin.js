2d2a6254cefa0fab1dd6cddb1e4983199c92d211
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-10-02 04:07:08 +0300

    Create .eslintrc.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,9 @@
-console.log("Welcome to Holberton School, what is your name? ");
+console.log('Welcome to Holberton School, what is your name? ');
 
-process.stdin.on("data", function (data) {
-    let name = data.toString().trim();
-    console.log(`Your name is: ${name}`);
-})
-process.stdin.on("end", function () {
-    console.log("This important software is now closing");
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  console.log(`Your name is: ${name}`);
+});
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
 });
