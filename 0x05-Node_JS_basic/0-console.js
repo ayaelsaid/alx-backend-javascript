22f968ef0c8b1f31b6de8df5111d084f5495d974
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-10-02 04:05:28 +0300

    Update 0-console.js

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,15 +1,4 @@
-/**
- * Logs a message to the console.
- * @param {string} message - The message to log.
- * @returns {void}
- */
 function displayMessage(message){
     console.log(message)
 }
-/**
- * Exports the displayMessage function.
- * @module displayMessage
- * @function
- */
-
 module.exports = displayMessage;
