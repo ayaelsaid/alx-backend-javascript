cd6facae7d8e75beb4da23c8340800d8efc627db
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-10-02 05:26:50 +0300

    Update 3-read_file_async.js

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,46 +1,40 @@
 const fs = require('fs');
 
 const countStudents = (file) => new Promise((resolve, reject) => {
-  if (!fs.existsSync(file) || !fs.statSync(file).isFile()) {
-    return reject(new Error('Cannot load the database'));
-  }
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+            return reject(new Error('Cannot load the database'));
+        }
 
-  fs.readFile(file, 'utf8', (err, data) => {
-    if (err) {
-      return reject(new Error('Cannot load the database'));
-    }
+        const lines = data.trim().split('\n').filter((line) => line.trim() !== '');
+        const students = lines.slice(1);
+        const totalStudents = `Number of students: ${students.length}`;
+        const countData = {};
 
-    const lines = data.split('\n').filter((line) => line.trim() !== '');
-    const students = lines.slice(1);
+        for (const student of students) {
+            const eachStudent = student.split(',');
+            const studentName = eachStudent[0].trim();
+            const fieldName = eachStudent[3].trim();
 
-    const totalStudents = `Number of students: ${students.length}`;
-    const countData = {};
+            if (!countData[fieldName]) {
+                countData[fieldName] = { count: 0, names: [] };
+            }
+            countData[fieldName].count += 1;
+            countData[fieldName].names.push(studentName);
+        }
 
-    for (const student of students) {
-      const eachStudent = student.split(',');
-      const studentName = eachStudent[0].trim();
-      const fieldName = eachStudent[3].trim();
+        // Log total students
+        console.log(totalStudents);
 
-      if (!countData[fieldName]) {
-        countData[fieldName] = { count: 0, names: [] };
-      }
-      countData[fieldName].count += 1;
-      countData[fieldName].names.push(studentName);
-    }
+        // Log each field's student data
+        for (const field in countData) {
+            if (Object.prototype.hasOwnProperty.call(countData, field)) {
+                console.log(`Number of students in ${field}: ${countData[field].count}. List: ${countData[field].names.join(', ')}`);
+            }
+        }
 
-    // Log total students
-    console.log(totalStudents);
-
-    // Log each field's student data
-    for (const field in countData) {
-      if (Object.prototype.hasOwnProperty.call(countData, field)) {
-        console.log(`Number of students in ${field}: ${countData[field].count}. List: ${countData[field].names.join(', ')}`);
-      }
-      return resolve({ totalStudents, countData });
-
-    }
-  });
-}) // This return is implicit, and it's fine here.
-;
+        resolve({ totalStudents, countData });
+    });
+});
 
 module.exports = countStudents;
