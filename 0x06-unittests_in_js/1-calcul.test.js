3ca511f56f6af6abf5066c9f6595eef888f7b05d
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-10-04 02:59:45 +0300

    Update 1-calcul.test.js

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,24 +1,24 @@
 const assert = require('assert');
 const calculateNumber = require('./1-calcul');
 
-describe('calculateNumber', () => { // Added quotes for the description
+describe('calculateNumber', () => {
     it('should return the sum of rounded numbers', () => {
         assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
     });
-it('should return result of sub ', () => {
 
+    it('should return result of subtraction', () => {
         assert.strictEqual(calculateNumber('SUBTRACT', 1, 3.7), -3);
-        });
-it('should return the result of the divided', () => {
-
-        assert.strictEqual(calculateNumber('DIVIDE', 6.2, 3.7), 2);
-        });
-it('should return to an error', () => {
-
-assert.strictEqual(calculateNumber('DIVIDE', 1.5, 0), 'Error');
     });
-it('should return "Error" for an unknown operation type', () => {
+
+    it('should return the result of the division', () => {
+        assert.strictEqual(calculateNumber('DIVIDE', 6.2, 3.7), 2);
+    });
+
+    it('should return an error for division by zero', () => {
+        assert.strictEqual(calculateNumber('DIVIDE', 1.5, 0), 'Error');
+    });
+
+    it('should return "Error" for an unknown operation type', () => {
         assert.strictEqual(calculateNumber('UNKNOWN', 1.4, 4.5), 'Error');
     });
-
 });
