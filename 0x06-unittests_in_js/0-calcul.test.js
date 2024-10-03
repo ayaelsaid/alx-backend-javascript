7cb86b6eb1808106c88e356a837ae6764014621c
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-10-04 02:40:06 +0300

    Update 0-calcul.test.js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,10 +1,20 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
-describe(calculateNumber, () =>{
-it('should return the sum of rounded numbers', () => {
+
+describe('calculateNumber', () => {
+    it('should return the sum of rounded numbers for (1, 3)', () => {
         assert.strictEqual(calculateNumber(1, 3), 4);
+    });
+
+    it('should return the sum of rounded numbers for (1, 3.7)', () => {
         assert.strictEqual(calculateNumber(1, 3.7), 5);
+    });
+
+    it('should return the sum of rounded numbers for (1.2, 3.7)', () => {
         assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    });
+
+    it('should return the sum of rounded numbers for (1.5, 3.7)', () => {
         assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     });
 });
