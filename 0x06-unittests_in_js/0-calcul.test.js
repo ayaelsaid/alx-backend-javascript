b0e30d9d95f2be80bba8a8e76cb0dd2aa97cf9d5
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-10-04 02:25:19 +0300

    Update 0-calcul.test.js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -7,3 +7,4 @@ it('should return the sum of rounded numbers', () => {
         assert.strictEqual(calculateNumber(1.2, 3.7), 5);
         assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     });
+});
