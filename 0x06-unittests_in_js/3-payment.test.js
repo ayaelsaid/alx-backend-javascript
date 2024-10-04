3ac8186064907bee4c7896937d6e0314e12f9a06
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-10-04 03:31:40 +0300

    Update 3-payment.test.js

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,4 +1,3 @@
-
 const chai = require('chai');
 const sinon = require('sinon');
 const Utils = require('./utils');
@@ -7,12 +6,11 @@ const sendPaymentRequestToApi = require('./main');
 const { expect } = chai;
 
 describe('sendPaymentRequestToApi', () => {
-    it('should call Utils.calculateNumber with the correct arguments', () => {
+    it('call model and return the result', () => {
         const spy = sinon.spy(Utils, 'calculateNumber');
-
         sendPaymentRequestToApi(100, 20);
-
-	    assert(spy.calledOnce);
-	    spy.restore();
+        expect(spy.calledOnce).to.be.true;
+        expect(spy.calledWith('SUM', 100, 20)).to.be.true;
+        spy.restore();
     });
 });
