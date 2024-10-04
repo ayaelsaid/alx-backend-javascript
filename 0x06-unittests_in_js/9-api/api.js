bd5ed0f587aad49bdc7f5b4b9648b8952a61836b
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-10-04 05:14:36 +0300

    Update api.js

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -3,14 +3,15 @@ const express = require('express');
 const app = express();
 const port = 7865;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Welcome to the payment system');
 });
 
 app.get('/cart/:id(\\d+)', (req, res) => {
-    res.send(`Payment methods for cart ${req.params}`);
+    res.send(`Payment methods for cart ${req.params.id}`);
 });
-
 app.listen(port, () => {
     console.log(`API available on localhost port ${port}`);
 });
