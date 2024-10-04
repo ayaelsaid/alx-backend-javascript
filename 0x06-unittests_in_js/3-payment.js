dc37e45984718cc8b6e303b212bd5c1ff334d8b6
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-10-04 03:22:40 +0300

    Update 3-payment.js

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,4 +1,8 @@
-function sendPaymentRequestToApi(totalAmount, totalShipping){
- const result =  Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is: ${result}`)
+const Utils = require('./utils');
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+    const result = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+    console.log(`The total is: ${result}`);
 }
+
+module.exports = sendPaymentRequestToApi;
