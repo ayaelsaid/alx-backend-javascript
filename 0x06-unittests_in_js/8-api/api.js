42c23a87b20bde3d7750e511fd413542dc32e0d1
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-10-04 04:38:43 +0300

    Update api.js

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 7865
-app.get('/', (__, res) => {
+app.get('/', (req, res) => {
     res.send('Welcome to the payment system');
 });
 
